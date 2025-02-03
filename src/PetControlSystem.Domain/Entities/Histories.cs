17f0ba6a9951f29860e082017489f809e52f0057
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-03 20:48:37 -0300

    create Histories

---
+++ src/PetControlSystem.Domain/Entities/Histories.cs
@@ -0,0 +1,8 @@
+﻿namespace PetControlSystem.Domain.Entities
+{
+    public class Histories : Entity
+    {
+        public List<Appointment>? Appointments { get; private set; }
+        public List<Order>? Order { get; private set; }
+    }
+}
