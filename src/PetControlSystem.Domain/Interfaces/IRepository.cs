90f2f9d2fdb105a8cb98417308e15dbf0762c041
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-03 21:20:22 -0300

    create IRepository

---
+++ src/PetControlSystem.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using PetControlSystem.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace PetControlSystem.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Add(TEntity obj);
+        Task<TEntity> GetById(Guid id);
+        IEnumerable<TEntity> GetAll();
+        Task Update(TEntity obj);
+        Task Remove(Guid id);
+        Task<int> SaveChanges();
+        Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate);
+    }
+}
