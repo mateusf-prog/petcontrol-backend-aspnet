22fbc37cd5c341ed3c6285e897a383496a6b3938
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-03 21:11:25 -0300

    Revert "create Histories"
    This reverts commit 17f0ba6a9951f29860e082017489f809e52f0057.

--- src/PetControlSystem.Domain/Entities/Histories.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PetControlSystem.Domain.Entities
-{
-    public class Histories : Entity
-    {
-        public List<Appointment>? Appointments { get; private set; }
-        public List<Order>? Order { get; private set; }
-    }
-}
