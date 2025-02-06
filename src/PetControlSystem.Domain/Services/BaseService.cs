3864f85ae5864457f679768c2b2f199750a82909
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-06 20:50:42 -0300

    implementing BaseService

--- src/PetControlSystem.Domain/Services/BaseService.cs
+++ src/PetControlSystem.Domain/Services/BaseService.cs
@@ -1,6 +1,13 @@
-﻿namespace PetControlSystem.Domain.Services
+﻿using FluentValidation;
+
+namespace PetControlSystem.Domain.Services
 {
     public abstract class BaseService
     {
-    }
+        protected bool ExecuteValidation<V, E>(V validation, E entity) where V : AbstractValidator<E>
+        {
+            var result = validation.Validate(entity);
+            return result.IsValid;
+        }
+}
 }
