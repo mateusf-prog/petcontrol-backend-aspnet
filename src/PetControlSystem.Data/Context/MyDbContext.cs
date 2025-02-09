9563a73cead592349a6d60dca3a4b01be2b7932c
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-09 14:41:32 -0300

    implement DbContext

---
+++ src/PetControlSystem.Data/Context/MyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Domain.Entities;
+
+namespace PetControlSystem.Data.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Appointment> Appointment { get; set; }
+        public DbSet<PetSupport> PetSupport { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                    .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
