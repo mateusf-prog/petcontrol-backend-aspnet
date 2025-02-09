abeea9b93505da6a92455d636ae0fec539862051
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-09 11:41:32 -0300

    change BaseService to use notifications

--- src/PetControlSystem.Domain/Services/BaseService.cs
+++ src/PetControlSystem.Domain/Services/BaseService.cs
@@ -1,18 +1,38 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
+using PetControlSystem.Domain.Notification;
 
 namespace PetControlSystem.Domain.Services
 {
     public abstract class BaseService
     {
+        private readonly INotificator _notification;
+
+        protected BaseService(INotificator notification)
+        {
+            _notification = notification;
+        }
+
+        protected void Notify(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+                Notify(error.ErrorMessage);
+        }
 
         public void Notify(string message)
         {
+            _notification.Handle(new Notificator(message));
         }
 
         protected bool ExecuteValidation<V, E>(V validation, E entity) where V : AbstractValidator<E>
         {
-            var result = validation.Validate(entity);
-            return result.IsValid;
+            var validator = validation.Validate(entity);
+
+            if (validator.IsValid) return true;
+
+            Notify(validator);
+
+            return validator.IsValid;
         }
 }
 }
