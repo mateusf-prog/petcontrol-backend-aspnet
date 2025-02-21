fb255dbc8ae6853ec599a2f0aca5f5901483453e
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-20 22:13:18 -0300

    create generic Repository

---
+++ src/PetControlSystem.Data/Repository/Repository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
+using System.Linq.Expressions;
+
+namespace PetControlSystem.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected DbContext Db;
+        protected DbSet<TEntity> DbSet;
+
+        public Repository(DbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task<TEntity?> GetById(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public Task Add(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Remove(Guid id)
+        {
+            DbSet.Remove(new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
