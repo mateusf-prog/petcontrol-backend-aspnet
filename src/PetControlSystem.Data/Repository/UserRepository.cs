5b9b253cf787337aebd00cdc0984eb75f2e41178
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-24 21:41:59 -0300

    Create UserRepository

---
+++ src/PetControlSystem.Data/Repository/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
+
+namespace PetControlSystem.Data.Repository
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(DbContext db) : base(db) { }
+    }
+}
