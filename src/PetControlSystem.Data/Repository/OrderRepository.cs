41ca9bb2f10e3ad10885fb8644ddfff282facb9c
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-24 21:42:34 -0300

    Create OrderRepository  and PetRepository

---
+++ src/PetControlSystem.Data/Repository/OrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
+
+namespace PetControlSystem.Data.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(DbContext db) : base(db) { }
+    }
+}
---
+++ src/PetControlSystem.Data/Repository/PetRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
+
+namespace PetControlSystem.Data.Repository
+{
+    public class PetRepository : Repository<Pet>, IPetRepository
+    {
+        public PetRepository(DbContext db) : base(db) { }
+    }
+}
