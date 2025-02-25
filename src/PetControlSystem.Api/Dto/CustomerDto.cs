20d9f33e10ea31b0f8dd44a695429205b0990179
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-24 22:25:27 -0300

    create OrderDto and CustomerDto

---
+++ src/PetControlSystem.Api/Dto/CustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetControlSystem.Api.Dto
+{
+    public class CustomerDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
+        public string? Name { get; private set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} is in an invalid format")]
+        public string? Email { get; private set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Phone(ErrorMessage = "The field {0} is in an invalid format")]
+        public string? Phone { get; private set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(14, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 11)]
+        public string? Document { get; private set; }
+
+        public AddressDto? AddressDto { get; set; }
+        public PetDto? PetDto { get; set; }
+        public List<OrderDto>? OrderDto { get; set; }
+    }
+}
---
+++ src/PetControlSystem.Api/Dto/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetControlSystem.Api.Dto
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Range(0.01, double.MaxValue , ErrorMessage = "The field {0} must be greater than {1}.")]
+        public decimal TotalPrice { get; set; }
+
+        public string? CustomerName { get; set; }
+
+        public List<ProductDto>? Products { get; set; }
+    }
+}
