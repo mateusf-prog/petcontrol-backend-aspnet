d0ec1b1b183f1921b37d3070f7deacd379691e85
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-25 19:51:15 -0300

    create pet and petsupport controller

---
+++ src/PetControlSystem.Api/Controllers/PetSupportsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetControlSystem.Api.Dto;
+
+namespace PetControlSystem.Api.Controllers
+{
+    [Route("api/pet-supports")]
+    public class PetSupportsController : MainController
+    {
+        public PetSupportsController() { }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PetSupportDto>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<PetSupportDto>> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PetSupportDto>> Create(PetSupportDto petSupportDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<PetSupportDto>> Update(Guid id, PetSupportDto petSupportDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<PetSupportDto>> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PetControlSystem.Api/Controllers/PetsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetControlSystem.Api.Dto;
+
+namespace PetControlSystem.Api.Controllers
+{
+    [Route("api/pets")]
+    public class PetsController : MainController
+    {
+        public PetsController() { }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PetDto>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<PetDto>> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PetDto>> Create(PetDto petDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<PetDto>> Update(Guid id, PetDto petDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<PetDto>> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
