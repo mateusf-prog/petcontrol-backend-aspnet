047daaa0792b24ad6ea171fc89ce953a0ffbc2de
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-25 19:51:41 -0300

    create address and appointment controller

---
+++ src/PetControlSystem.Api/Controllers/Addresses.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetControlSystem.Api.Dto;
+
+namespace PetControlSystem.Api.Controllers
+{
+    [Route("api/addresses")]
+    public class Addresses : MainController
+    {
+        public Addresses() { }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AddressDto>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<AddressDto>> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AddressDto>> Create(AddressDto addressDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<AddressDto>> Update(Guid id, AddressDto addressDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<AddressDto>> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PetControlSystem.Api/Controllers/AppointmentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetControlSystem.Api.Dto;
+
+namespace PetControlSystem.Api.Controllers
+{
+    [Route("api/appointments")]
+    public class AppointmentsController : MainController
+    {
+        public AppointmentsController() { }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppointmentDto>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<AppointmentDto>> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AppointmentDto>> Create(AppointmentDto appointmentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<AppointmentDto>> Update(Guid id, AppointmentDto appointmentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<AppointmentDto>> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
