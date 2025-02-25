e5f564b539045e881b917a061405a93a426495b3
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-24 21:42:23 -0300

    Create ProductRepository and PetSupportRepository

---
+++ src/PetControlSystem.Data/Repository/PetSupportRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
+
+namespace PetControlSystem.Data.Repository
+{
+    public class PetSupportRepository : Repository<PetSupport>, IPetSupportRepository
+    {
+        public PetSupportRepository(DbContext db) : base(db) { }
+    }
+}
---
+++ src/PetControlSystem.Data/Repository/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
+
+namespace PetControlSystem.Data.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(DbContext context) : base(context) { }
+    }
+}
