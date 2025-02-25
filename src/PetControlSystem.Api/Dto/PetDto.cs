ceec94c0caa3cce080c55b41fcc00e0de81df4f3
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-24 22:25:14 -0300

    create PetSupportDto and PetDto

---
+++ src/PetControlSystem.Api/Dto/PetDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetControlSystem.Api.Dto
+{
+    public class PetDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(80, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string? Weight { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(1, 3, ErrorMessage = "The field {0} must be between {1} and {2}")]
+        public int Type { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(1, 2, ErrorMessage = "The field {0} must be between {1} and {2}")]
+        public int Gender { get; set; }
+
+        public Guid CustomerId { get; set; }
+    }
+}
---
+++ src/PetControlSystem.Api/Dto/PetSupportDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetControlSystem.Api.Dto
+{
+    public class PetSupportDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 5)]
+        public string? Name { get; private set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(0, 9999, ErrorMessage = "The field {0} must be between {1} and {2}")]
+        public decimal? SmallDogPrice { get; private set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(0, 9999, ErrorMessage = "The field {0} must be between {1} and {2}")]
+        public decimal? MediumDogPrice { get; private set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(0, 9999, ErrorMessage = "The field {0} must be between {1} and {2}")]
+        public decimal? LargeDogPrice { get; private set; }
+    }
+}
