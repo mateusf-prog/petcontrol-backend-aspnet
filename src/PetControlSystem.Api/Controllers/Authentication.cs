6d5eb02a68de2959e7025a2eb7be38c47ec3a90c
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-27 21:07:18 -0300

    create Authentication controller

---
+++ src/PetControlSystem.Api/Controllers/Authentication.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetControlSystem.Api.Dto;
+using PetControlSystem.Api.Mappers;
+using PetControlSystem.Domain.Interfaces;
+
+namespace PetControlSystem.Api.Controllers
+{
+    [Route("api/login")]
+    public class Authentication : MainController
+    {
+        private readonly IUserService _userService;
+
+        public Authentication(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public async Task<ActionResult<UserDto>> Authenticate([FromBody]UserDto user)
+        {
+            var result = await _userService.Login(user.ToEntity());
+
+            return Ok(result);
+        }
+    }
+}
