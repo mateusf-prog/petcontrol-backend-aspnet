d53de7861245dd9dabe75ecc4091f61840aeeff4
Mateus Fonseca <mateus102006@hotmail.com>
2025-02-27 21:06:14 -0300

    create tokenservice

---
+++ src/PetControlSystem.Api/Security/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using PetControlSystem.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PetControlSystem.Api.Security
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var secret = Encoding.ASCII.GetBytes(_configuration["AppSettings:Secret"]!);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                [
+                        new Claim(ClaimTypes.Name, user.Email!.ToString()),
+                        new Claim(ClaimTypes.Role, user.Type.ToString())
+                ]),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
