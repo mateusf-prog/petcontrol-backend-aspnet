bb70725ef6ad9a1a487f2c2c7c76e80dd1a64a27
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-01 15:24:33 -0300

    create LoginDto

---
+++ src/PetControlSystem.Api/Dto/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetControlSystem.Api.Dto
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Password { get; set; }
+    }
+}
