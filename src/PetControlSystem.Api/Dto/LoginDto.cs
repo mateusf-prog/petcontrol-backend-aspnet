4dbed351043eead2a36626f1f42ab35f02abefe2
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-01 17:34:01 -0300

    update LoginDto

--- src/PetControlSystem.Api/Dto/LoginDto.cs
+++ src/PetControlSystem.Api/Dto/LoginDto.cs
@@ -5,6 +5,7 @@ namespace PetControlSystem.Api.Dto
     public class LoginDto
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress]
         public string Email { get; set; }
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Password { get; set; }
