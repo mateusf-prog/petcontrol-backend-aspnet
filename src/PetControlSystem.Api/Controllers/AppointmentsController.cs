d65ca9ba6e15296d7b5454070bd5aabb3a7b72b2
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-02 11:08:25 -0300

    implement AppointmentsController

--- src/PetControlSystem.Api/Controllers/AppointmentsController.cs
+++ src/PetControlSystem.Api/Controllers/AppointmentsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PetControlSystem.Api.Dto;
+using PetControlSystem.Api.Mappers;
 using PetControlSystem.Domain.Interfaces;
 using PetControlSystem.Domain.Notifications;
+using System.Net;
 
 namespace PetControlSystem.Api.Controllers
 {
@@ -9,40 +11,50 @@ namespace PetControlSystem.Api.Controllers
     public class AppointmentsController : MainController
     {
         private readonly IAppointmentService _service;
+        private readonly IAppointmentRepository _repository;
 
-        public AppointmentsController(IAppointmentService service, INotificator notificator) : base(notificator)
+        public AppointmentsController(IAppointmentService service,IAppointmentRepository repository, INotificator notificator) : base(notificator)
         {
             _service = service;
+            _repository = repository;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppointmentDto>>> GetAll()
         {
-            throw new NotImplementedException();
+            var result = await _repository.GetAll();
+            return result.Select(a => a.ToDto()).ToList();
         }
 
         [HttpGet("{id:guid}")]
         public async Task<ActionResult<AppointmentDto>> GetById(Guid id)
         {
-            throw new NotImplementedException();
+            var result = await _repository.GetById(id);
+            if (result is null) return NotFound();
+            return result.ToDto();
         }
 
         [HttpPost]
-        public async Task<ActionResult<AppointmentDto>> Create(AppointmentDto appointmentDto)
+        public async Task<ActionResult<AppointmentDto>> Create(AppointmentDto input)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+            await _service.Add(input.ToEntity());
+            return CustomResponse(HttpStatusCode.Created, input);
         }
 
         [HttpPut("{id:guid}")]
-        public async Task<ActionResult<AppointmentDto>> Update(Guid id, AppointmentDto appointmentDto)
+        public async Task<ActionResult<AppointmentDto>> Update(Guid id, AppointmentDto input)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+            await _service.Update(id, input.ToEntity());
+            return CustomResponse(HttpStatusCode.NoContent, input);
         }
 
         [HttpDelete("{id:guid}")]
         public async Task<ActionResult<AppointmentDto>> Delete(Guid id)
         {
-            throw new NotImplementedException();
+            await _service.Delete(id);
+            return CustomResponse(HttpStatusCode.NoContent);
         }
     }
 }
