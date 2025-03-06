f783619b2f0cc22fe82b70b27d16a4aa781af253
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-06 00:37:32 -0300

    update OrderService

--- src/PetControlSystem.Domain/Services/OrderService.cs
+++ src/PetControlSystem.Domain/Services/OrderService.cs
@@ -8,24 +8,57 @@ namespace PetControlSystem.Domain.Services
     public class OrderService : BaseService, IOrderService
     {
         private readonly IOrderRepository _repository;
+        private readonly IProductRepository _productRepository;
+        private readonly ICustomerRepository _customerRepository;
 
         public OrderService(IOrderRepository repository,
-                            INotificator notificator) : base(notificator)
+                            INotificator notificator,
+                            IProductRepository productRepository,
+                            IProductService productService,
+                            ICustomerRepository customerRepostiory) : base(notificator)
         {
             _repository = repository;
+            _productRepository = productRepository;
+            _customerRepository = customerRepostiory;
+
         }
 
         public async Task Add(Order input)
         {
             if (!ExecuteValidation(new OrderValidation(), input)) return;
 
-            if (await _repository.GetById(input.Id) != null) 
+            if (await _repository.GetById(input.Id) != null)
             {
                 Notify("There is already an order with this ID");
                 return;
             }
 
-            await _repository.Add(input);
+            if (await _customerRepository.GetById(input.CustomerId) is null)
+            {
+                Notify($"Customer not found - ID {input.CustomerId}");
+                return;
+            }
+
+            foreach (var orderProduct in input.OrderProducts)
+            {
+                var product = await _productRepository.GetById(orderProduct.ProductId);
+
+                if (product.Stock < orderProduct.Quantity)
+                {
+                    Notify($"Insufficient stock of product {product.Name}");
+                    return;
+                }
+
+                if (product is null)
+                {
+                    Notify($"Product not found - ID {product.Id}");
+                    return;
+                }
+
+                product.Update(product.Name, product.Price, product.Stock - orderProduct.Quantity, product.Description);
+                await _productRepository.Update(product);
+                await _repository.Add(input);
+            }
         }
 
         public async Task Update(Guid id, Order input)
@@ -39,7 +72,7 @@ namespace PetControlSystem.Domain.Services
                 return;
             }
 
-            result.Update(input.Customer, input.Products);
+            result.Update(input.Customer, input.OrderProducts);
 
             await _repository.Update(result);
         }
