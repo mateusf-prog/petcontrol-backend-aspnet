690ededdaec546f0830ea156aef6dee6910369ac
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-08 21:14:58 -0300

    create new mapper and new dto AppointmentPetSupport

---
+++ src/PetControlSystem.Api/Dto/AppointmentPetSupportDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PetControlSystem.Api.Dto
+{
+    public class AppointmentPetSupportDto
+    {
+        public Guid Id { get; set; }
+        public Guid AppointmentId { get; set; }
+        public Guid PetSupportId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/PetControlSystem.Api/Mappers/AppointmentPetSupportMapper.cs
@@ -0,0 +1,26 @@
+﻿using PetControlSystem.Api.Dto;
+using PetControlSystem.Domain.Entities;
+
+namespace PetControlSystem.Api.Mappers
+{
+    public static class AppointmentPetSupportMapper
+    {
+        public static AppointmentPetSupport ToEntity(this AppointmentPetSupportDto dto)
+        {
+            return new AppointmentPetSupport(
+                dto.AppointmentId,
+                dto.PetSupportId,
+                dto.Price);
+        }
+
+        public static AppointmentPetSupportDto ToDto(this AppointmentPetSupport entity)
+        {
+            return new AppointmentPetSupportDto
+            {
+                Id = entity.Id,
+                PetSupportId = entity.PetSupportId,
+                Price = entity.Price,
+            };
+        }
+    }
+}
