21848662a81e2ce780bf02801ad896555543400e
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-08 21:12:28 -0300

    create AppointmentPetSupportRepository

---
+++ src/PetControlSystem.Data/Repository/AppointmentPetSupportRepository.cs
@@ -0,0 +1,13 @@
+﻿using PetControlSystem.Data.Context;
+using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
+
+namespace PetControlSystem.Data.Repository
+{
+    public class AppointmentPetSupportRepository : Repository<AppointmentPetSupport>, IAppointmentPetSupportRepository
+    {
+        public AppointmentPetSupportRepository(MyDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/PetControlSystem.Domain/Interfaces/IAppointmentPetSupportRepository.cs
@@ -0,0 +1,8 @@
+﻿using PetControlSystem.Domain.Entities;
+
+namespace PetControlSystem.Domain.Interfaces
+{
+    public interface IAppointmentPetSupportRepository : IRepository<AppointmentPetSupport>
+    {
+    }
+}
