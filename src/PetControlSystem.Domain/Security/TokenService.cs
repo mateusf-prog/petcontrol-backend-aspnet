9d9ba3a8b44df3137ac3d1caacfd9d8f0aa5043b
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-13 22:34:15 -0300

    feat: implement ITokenService in TokenService and update token generation logic to use IdentityUser

--- src/PetControlSystem.Domain/Security/TokenService.cs
+++ src/PetControlSystem.Domain/Security/TokenService.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using PetControlSystem.Domain.Entities;
+using PetControlSystem.Domain.Interfaces;
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Text;
 
 namespace PetControlSystem.Domain.Security
 {
-    public class TokenService
+    public class TokenService : ITokenService
     {
         private readonly IConfiguration _configuration;
 
@@ -16,21 +16,18 @@ namespace PetControlSystem.Domain.Security
             _configuration = configuration;
         }
 
-        public string GenerateToken(User user)
+        public string GenerateToken(IdentityUser user)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var secret = Encoding.ASCII.GetBytes(_configuration["AppSettings:Secret"]!);
-            var tokenDescriptor = new SecurityTokenDescriptor
+            var key = Encoding.ASCII.GetBytes(_configuration["JwtSecurity:Secret"]);
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(
-                [
-                        new Claim(ClaimTypes.Name, user.Email!.ToString()),
-                        new Claim(ClaimTypes.Role, user.Type.ToString())
-                ]),
-                Expires = DateTime.UtcNow.AddHours(2),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+                Issuer = _configuration["JwtSecurity:Issuer"],
+                Audience = _configuration["JwtSecurity:Audience"],
+                Expires = System.DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
             return tokenHandler.WriteToken(token);
         }
     }
