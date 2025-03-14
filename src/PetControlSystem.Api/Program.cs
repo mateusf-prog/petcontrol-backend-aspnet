f5381ebfdf3ac12f44cb1d837dd19c7700d9c8c1
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-13 21:13:09 -0300

    refactor: remove unused Microsoft.AspNetCore.Identity namespace from Program.cs

--- src/PetControlSystem.Api/Program.cs
+++ src/PetControlSystem.Api/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PetControlSystem.Data.Context;
 using PetControlSystem.Api.Extensions;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
