d0ddf80f1fec58bb65c9a83d52ddaea158edfa7f
Mateus Fonseca <mateus102006@hotmail.com>
2025-03-20 22:53:41 -0300

    feat: enhance TokenService to include error handling and notification on token generation failure

--- src/PetControlSystem.Domain/Services/TokenService.cs
+++ src/PetControlSystem.Domain/Services/TokenService.cs
@@ -2,33 +2,43 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using PetControlSystem.Domain.Interfaces;
+using PetControlSystem.Domain.Notifications;
+using PetControlSystem.Domain.Services;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 
 namespace PetControlSystem.Domain.Security
 {
-    public class TokenService : ITokenService
+    public class TokenService : BaseService, ITokenService
     {
         private readonly IConfiguration _configuration;
 
-        public TokenService(IConfiguration configuration)
+        public TokenService(IConfiguration configuration, INotificator notificator) : base(notificator)
         {
             _configuration = configuration;
         }
 
         public string GenerateToken(IdentityUser user)
         {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_configuration["JwtSecurity:Secret"]);
-            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            try
             {
-                Issuer = _configuration["JwtSecurity:Issuer"],
-                Audience = _configuration["JwtSecurity:Audience"],
-                Expires = DateTime.UtcNow.AddHours(3),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            });
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["JwtSecurity:Secret"]);
+                var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = _configuration["JwtSecurity:Issuer"],
+                    Audience = _configuration["JwtSecurity:Audience"],
+                    Expires = DateTime.UtcNow.AddHours(3),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                });
 
-            return tokenHandler.WriteToken(token);
+                return tokenHandler.WriteToken(token);
+            } 
+            catch (Exception)
+            {
+                Notify("Error generating token");
+                return string.Empty;
+            }
         }
     }
 }
